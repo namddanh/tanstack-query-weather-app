340f07a4b8d973f8df29df65b64fac5f53c0e456
Nam D. <namddanh@gmail.com>
2026-02-04 23:39:51 -0500

    fetching current data

---
+++ src/components/loading-skeleton.tsx
@@ -0,0 +1,23 @@
+import { Skeleton } from "./ui/skeleton";
+
+const WeatherSkeleton = () => {
+  return (
+    <div className="space-y-6">
+      <div className="gap-6">
+        {/* FAVOURITES */}
+        <Skeleton className="h-75 w-full rounded-lg" />
+      </div>
+      <div className="grid gap-6">
+        <Skeleton className="h-75 w-full rounded-lg" />
+        <Skeleton className="h-75 w-full rounded-lg" />
+        <div className="grid gap-6 md:grid-cols-2">
+          <Skeleton className="h-75 w-full rounded-lg" />
+          <Skeleton className="h-75 w-full rounded-lg" />
+        </div>
+      </div>
+      WeatherSkeleton
+    </div>
+  );
+};
+
+export default WeatherSkeleton;
--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,7 +1,79 @@
-import React from "react";
+import WeatherSkeleton from "@/components/loading-skeleton";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import { useGeoLocation } from "@/hooks/useGeolocation";
+import { AlertTriangle, MapPin, RefreshCw } from "lucide-react";
 
 const WeatherDashboard = () => {
-  return <div>WeatherDashboard</div>;
+  const {
+    coordinates,
+    error: locationError,
+    getLocation,
+    isLoading: locationLoading,
+  } = useGeoLocation();
+
+  console.log(coordinates);
+
+  const handleRefresh = () => {
+    getLocation();
+    if (coordinates) {
+      // reload weather data
+    }
+  };
+
+  if (locationLoading) {
+    return <WeatherSkeleton />;
+  }
+
+  if (locationError) {
+    return (
+      <Alert variant="destructive" className="max-w-md">
+        <AlertTriangle />
+        <AlertTitle>Location Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>{locationError}</p>
+          <Button onClick={getLocation} variant={"outline"} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4" />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  if (!coordinates) {
+    return (
+      <Alert variant="destructive" className="max-w-md">
+        <AlertTitle>Location Required</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>Please enable location access to see your local weather.</p>
+          <Button onClick={getLocation} variant={"outline"} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4" />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  return (
+    <div className="space-y-4">
+      {/* Favourite Cities */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+        <Button
+          variant={"outline"}
+          size={"icon"}
+          onClick={handleRefresh}
+          // disabled
+        >
+          <RefreshCw className="h-4 w-4" />
+        </Button>
+      </div>
+
+      {/* Current and Hourly weather */}
+    </div>
+  );
 };
 
 export default WeatherDashboard;
